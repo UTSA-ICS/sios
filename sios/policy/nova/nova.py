059f804b08765554beae9111833de3b7bfefce20
fpatwa <farhan.patwa@utsa.edu>
2013-08-08 11:57:46 -0500

    removed debugging print statement

--- sios/policy/nova/nova.py
+++ sios/policy/nova/nova.py
@@ -60,7 +60,6 @@ def init():
             _POLICY_PATH = CONF.find_file(_POLICY_PATH)
         if not _POLICY_PATH:
             raise exception.ConfigNotFound(path=CONF.policy_file_nova)
-    print 'DANG READ%%%%%%%%%%%%%%%%%%%%%%%%%%'
     utils.read_cached_file(_POLICY_PATH, _POLICY_CACHE,
                            reload_func=_set_rules)
 
@@ -92,18 +91,14 @@ def enforce(context, action, target, do_raise=True):
            authorized, and the exact value False if not authorized and
            do_raise is False.
     """
-    print 'CALLING POLICY CHECK NOW##################### 1'
     init()
-    print 'CALLING POLICY CHECK NOW##################### 2'
 
     credentials = context.to_dict()
-    print 'CALLING POLICY CHECK NOW##################### 3'
 
     # Add the exception arguments if asked to do a raise
     extra = {}
     if do_raise:
         extra.update(exc=exception.PolicyNotAuthorized, action=action)
-    print 'CALLING POLICY CHECK NOW#####################'
     return policy.check(action, target, credentials, **extra)
 
 
