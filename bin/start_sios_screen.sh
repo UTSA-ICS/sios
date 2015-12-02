360f781a2a07d8bb73192c37216155afdcb43081
Farhan Patwa <farhan.patwa@utsa.edu>
2015-12-02 19:10:02 +0000

    New startup script

---
+++ bin/start_sios_screen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NL=`echo -ne '\015'`
+SIOS_CMD="cd /opt/stack/sios; /opt/stack/sios/bin/sios-api --config-file=/etc/sios/sios-api.conf || touch \"/opt/stack/status/stack/sios-api.failure\""
+SCREEN=$(which screen)
+if [[ -n "$SCREEN" ]]; then
+    SESSION=$(screen -ls | awk '/[0-9].stack/ { print $1 }')
+    if [[ -n "$SESSION" ]]; then
+        screen -S $SESSION -p sios -X logfile $SIOS_LOGFILE
+        screen -S $SESSION -p sios -X log on
+        screen -S $SESSION -p sios -X stuff "$SIOS_CMD $NL"
+    fi
+fi
+
