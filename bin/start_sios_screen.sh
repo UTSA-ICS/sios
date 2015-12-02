6c42545c144f02afeb8f873184942615d9a5bddb
Farhan Patwa <farhan.patwa@utsa.edu>
2015-12-02 19:49:33 +0000

    Updated start script

--- bin/start_sios_screen.sh
+++ bin/start_sios_screen.sh
@@ -2,6 +2,7 @@
 
 NL=`echo -ne '\015'`
 SIOS_CMD="cd /opt/stack/sios; /opt/stack/sios/bin/sios-api --config-file=/etc/sios/sios-api.conf || touch \"/opt/stack/status/stack/sios-api.failure\""
+SIOS_LOGFILE="/opt/stack/logs/screen/screen-sios.log"
 SCREEN=$(which screen)
 if [[ -n "$SCREEN" ]]; then
     SESSION=$(screen -ls | awk '/[0-9].stack/ { print $1 }')
